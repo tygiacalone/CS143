9555f6fd186fafb7dcb6121911533c12a2e8a5a1
chloedrazen <chloedrazen@ucla.edu>
2015-10-19 20:54:01 -0700

    dglkajlg

--- create.sql
+++ create.sql
@@ -1,52 +1,80 @@
 
 CREATE TABLE Movie (
-       id int,
+       id int NOT NULL,
        title varchar(100),
        year int,
        rating varchar(10),
-       company varchar(50)
+       company varchar(50),
+       /* id is primary key */
+       PRIMARY KEY(id),
+       /* Every movie must have a title */
+       CHECK (title IS NOT NULL),
+       /* Every movie must have a release year */
+       CHECK (year IS NOT NULL)
 );
 
 CREATE TABLE Actor (
-       id int,
+       id int NOT NULL,
        last varchar(20),
        first varchar(20),
        sex varchar(6),
        dob date,
-       dod date
+       dod date,
+       /* id is primary key */
+       PRIMARY KEY(id),
+       /* Every actor must be born before he/she dies */
+       CHECK (dob < dod),
+       /* Every actor must have a birth date */
+       CHECK (dob IS NOT NULL)
 );
 
 CREATE TABLE Director (
-       id int,
+       id int NOT NULL,
        last varchar(20),
        first varchar(20),
        dob date,
-       dod date
+       dod date,
+       /* id is primary key */
+       PRIMARY KEY(id),
+       /* Every director must be born before he/she dies */
+       CHECK (dob < dod),
+       /* Every director must have a birth date */
+       CHECK (dob IS NOT NULL)
 );
 
 CREATE TABLE MovieGenre (
        mid int,
-       genre varchar(20)
-);
+       genre varchar(20),
+        /* Every movie must be in database */
+       FOREIGN KEY (mid) references Movie(id)
+) ENGINE=INNODB;;
 
 CREATE TABLE MovieDirector (
        mid int,
-       did int
-);
+       did int,
+       /* Every movie and director must be in database */
+       FOREIGN KEY (mid) references Movie(id),
+       FOREIGN KEY (did) references Director(id)
+) ENGINE=INNODB;
 
 CREATE TABLE MovieActor (
        mid int,
        aid int,
-       role varchar(50)
-);
+       role varchar(50),
+       /* Every movie and actor must be in database */
+       FOREIGN KEY (mid) references Movie(id),
+       FOREIGN KEY (aid) references Actor(id)
+) ENGINE=INNODB;
 
 CREATE TABLE Review (
        name varchar(20),
        time timestamp,
        mid int,
        rating int,
-       comment varchar(500)
-);
+       comment varchar(500),
+       /* Every movie must be in database */
+       FOREIGN KEY (mid) references Movie(id)
+) ENGINE=INNODB;
 
 CREATE TABLE MaxPersonID (
        id int
--- queries.sql
+++ queries.sql
@@ -1,6 +1,32 @@
 
-SELECT CONCAT (first, ' ', last) AS 'DieAnotherDayActos'
+/*  Find the names of all the actors in the movie 'Die Another Day'. 
+Please also make sure actor names are in this format:  <firstname> <lastname>   
+(seperated by single space). */
+SELECT CONCAT (first, ' ', last) AS "Actors in Die Another Day"
 FROM Actor
 JOIN MovieActor ON Actor.id = MovieActor.aid
 JOIN Movie ON MovieActor.mid = Movie.id
-WHERE Movie.title = 'Die Another Day'
+WHERE Movie.title = 'Die Another Day';
+
+
+/*Find the count of all the actors who acted in multiple movies. */
+SELECT SUM(count1) AS "Total Actors in Multuple Movies" FROM (
+	SELECT COUNT(*) AS count1
+	FROM MovieActor
+	GROUP BY aid
+	HAVING COUNT(*) >= 2 
+) a;
+
+/*
+SELECT COUNT(*)
+FROM (
+	SELECT Actor.id
+	FROM Actor
+	JOIN MovieActor ON Actor.id = MovieActor.aid
+	GROUP BY Actor.id
+	HAVING COUNT(MovieActor.aid) > 1
+) as tmp;
+
+SELECT COUNT(id)
+FROM Actor;
+*/
