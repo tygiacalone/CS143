61db88b291dcfcc9d865c46ea5107883a5d6e077
Ty Giacalone <tygiacalone@gmail.com>
2015-11-15 18:11:17 -0800

    Remove offset from nonleaf split func

--- bruinbase/BTreeNode.cc
+++ bruinbase/BTreeNode.cc
@@ -484,7 +484,7 @@ RC BTNonLeafNode::insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, in
     int count1 = 0;
     while (count1 < maxNumKeys) {
         nEntry tmp;
-        memcpy(&tmp,  sizeof(PageId)+buffer + count1 * sizeof(nEntry), sizeof(nEntry));
+        memcpy(&tmp, buffer + count1 * sizeof(nEntry), sizeof(nEntry));
 
         cout << "key: " << tmp.key << endl;
         cout << "pid: " << tmp.pid << endl;
@@ -500,7 +500,7 @@ RC BTNonLeafNode::insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, in
 
     int keyCount = getKeyCount();
     int half = maxNumKeys/2;
-    ;
+
     int offset = half * sizeof(nEntry);
     int max = maxNumKeys * sizeof(nEntry);
     int incr = sizeof(nEntry);
@@ -510,7 +510,7 @@ RC BTNonLeafNode::insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, in
     while(offset < max) {
 
         nEntry * siblingNode = new nEntry;
-        memcpy(siblingNode, sizeof(PageId)+buffer+offset, sizeof(nEntry));
+        memcpy(siblingNode, buffer+offset, sizeof(nEntry));
 
         //cout << "key: " << siblingNode->key << endl;
         if(firstLoop) {
@@ -538,7 +538,7 @@ RC BTNonLeafNode::insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, in
     cout << "\nAfter: " << endl;\
     while (count < maxNumKeys) {
         nEntry tmp;
-        memcpy(&tmp, sizeof(PageId)+ buffer + count * sizeof(nEntry), sizeof(nEntry));
+        memcpy(&tmp, buffer + count * sizeof(nEntry), sizeof(nEntry));
 
         cout << "key: " << tmp.key << endl;
         cout << "pid: " << tmp.pid << endl;
@@ -550,7 +550,7 @@ RC BTNonLeafNode::insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, in
     cout << "\nSibling: " << endl;
     while (count < maxNumKeys) {
         nEntry tmp;
-        memcpy(&tmp, sizeof(PageId)+sibling.buffer + count * sizeof(nEntry), sizeof(nEntry));
+        memcpy(&tmp, sibling.buffer + count * sizeof(nEntry), sizeof(nEntry));
 
         cout << "key: " << tmp.key << endl;
         cout << "pid: " << tmp.pid << endl;
